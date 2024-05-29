2a0966a7e86c5af124d1aec35015e305bb1ce85a
kangsan1023 <kangsan9924@gmail.com>
2024-05-29 11:41:34 +0900

    [San]feat. board관련 mapper, vo, dao 생성

---
+++ gorang/src/main/java/com/kh/gorang/board/model/vo/Board.java
@@ -0,0 +1,26 @@
+package com.kh.gorang.board.model.vo;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Board {
+	private int boardNo;
+	private String boardTitle;
+	private String boardContent;
+	private Date boardCreateDate;
+	private int boardViews;
+	private String boardTag;
+	private int boardVote;
+	private char status;
+	private int memberNo;
+}
