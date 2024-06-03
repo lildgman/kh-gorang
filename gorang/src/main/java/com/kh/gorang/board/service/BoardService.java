43fa47380f8ed36f9c4514e405e70cc6dde52f6b
kangsan1023 <kangsan9924@gmail.com>
2024-06-03 15:50:23 +0900

    [San]feat. 게시판 연결중...

--- gorang/src/main/java/com/kh/gorang/board/service/BoardService.java
+++ gorang/src/main/java/com/kh/gorang/board/service/BoardService.java
@@ -1,7 +1,21 @@
 package com.kh.gorang.board.service;
 
+import java.util.ArrayList;
+
 import com.kh.gorang.board.model.vo.Board;
+import com.kh.gorang.common.vo.PageInfo;
 
 public interface BoardService{
+	//게시글 총 갯수 가져오기
+	int selectListCount();
+	
+	//게시글 리스트 조회(게시글메인)
+	ArrayList<Board>selectList(PageInfo pi);
+	
+	
+	//상세 게시글 조회
+	Board selectBoard(int boardNo);
+	
+	//게시글 추가
 	int insertBoard(Board board);
 }
--- gorang/src/main/java/com/kh/gorang/board/service/BoardServiceImpl.java
+++ gorang/src/main/java/com/kh/gorang/board/service/BoardServiceImpl.java
@@ -1,11 +1,17 @@
 package com.kh.gorang.board.service;
 
+import java.util.ArrayList;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kh.gorang.board.model.dao.BoardDao;
 import com.kh.gorang.board.model.vo.Board;
+import com.kh.gorang.common.vo.PageInfo;
+
+
+
 
 @Service
 public class BoardServiceImpl implements BoardService{
@@ -15,6 +21,21 @@ public class BoardServiceImpl implements BoardService{
 	
 	@Autowired
 	private BoardDao boardDao;
+	
+	@Override
+	public int selectListCount() {
+		int count = boardDao.selectListCount(sqlSession);
+		return count;
+	}
+	@Override
+	public ArrayList<Board> selectList(PageInfo pi){
+		return boardDao.selectList(sqlSession, pi);
+	}
+	
+	@Override
+	public Board selectBoard(int boardNo) {
+		return boardDao.selectBoard(sqlSession, boardNo);
+	}
 	
 	@Override
 	public int insertBoard(Board board) {
