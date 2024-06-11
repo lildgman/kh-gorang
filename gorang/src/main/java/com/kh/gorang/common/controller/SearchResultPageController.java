056046f808cde198d8f1f917ec1eb063be7cb44f
lildgman <humanorc2357@gmail.com>
2024-06-11 15:44:15 +0900

    [gun] feat 검색기능 구현중

--- gorang/src/main/java/com/kh/gorang/common/controller/SearchResultPageController.java
+++ gorang/src/main/java/com/kh/gorang/common/controller/SearchResultPageController.java
@@ -1,13 +1,13 @@
 package com.kh.gorang.common.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SearchResultPageController {
-	@RequestMapping("result.sr")
+	
+	@GetMapping("search.re")
 	public String  SearchResultPage() {
 		return "common/searchResultPage";
-		
 	}
 }
