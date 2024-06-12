2b32c71bc31f2fdf48563598598cb940542f5058
lildgman <humanorc2357@gmail.com>
2024-06-12 12:14:20 +0900

    [gun] fix LogginAOP

--- gorang/src/main/java/com/kh/gorang/recipe/model/dao/OdgRecipeDao.java
+++ gorang/src/main/java/com/kh/gorang/recipe/model/dao/OdgRecipeDao.java
@@ -6,12 +6,19 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.kh.gorang.recipe.model.vo.Recipe;
+import com.kh.gorang.shopping.model.vo.Product;
 
 @Repository
 public class OdgRecipeDao {
 
+	// 최근 레시피 조회
 	public ArrayList<Recipe> getRecentRecipe(SqlSessionTemplate sqlSession) {
 		return (ArrayList)sqlSession.selectList("odgRecipeMapper.selectRecentRecipe");
+	}
+
+	// 상품 조회
+	public ArrayList<Product> getProductList(SqlSessionTemplate sqlSession) {
+		return (ArrayList)sqlSession.selectList("odgRecipeMapper.selectProduct");
 	}
 
 }
